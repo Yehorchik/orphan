bfd0a6b055adb632db0e6da5215f0019d919452e
Robin <robert.taku.hartley@gmail.com>
2019-01-31 16:38:29 -0800

    updating

--- public/src/app/stories/stories.component.ts
+++ public/src/app/stories/stories.component.ts
@@ -6,38 +6,31 @@ import { HttpService } from '../http.service';
   styleUrls: ['./stories.component.css']
 })
 export class StoriesComponent implements OnInit {
-  users : any =[];
-  hello : any
-  like =false;
-  constructor(private _httpService: HttpService) { }
+  users: any = [];
+  hello: any;
+  like = false;
+  constructor(private _httpService: HttpService) {}
 
   ngOnInit() {
     this.allStories();
-  
   }
-   allStories(){
-     this._httpService.getAllStories()
-     .subscribe(result =>{
-        this.users = result;
-        console.log(this.users);
-     })
-   }
-   addLike(id){
-    if(this.like == false){
-      this._httpService.addOneLike(id,this.hello)
-      .subscribe(result =>{
+  allStories() {
+    this._httpService.getAllStories().subscribe(result => {
+      this.users = result;
+      console.log(this.users);
+    });
+  }
+  addLike(id) {
+    if (this.like === false) {
+      this._httpService.addOneLike(id, this.hello).subscribe(result => {
         this.allStories();
-        this.like =true;
-      })
-    }
-    else{
-      this._httpService.removeOneLike(id,this.hello)
-      .subscribe(result =>{
+        this.like = true;
+      });
+    } else {
+      this._httpService.removeOneLike(id, this.hello).subscribe(result => {
         this.allStories();
-        this.like =false;
-      })
+        this.like = false;
+      });
     }
-   
-   }
-
+  }
 }
